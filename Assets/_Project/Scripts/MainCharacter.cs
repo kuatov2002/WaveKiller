5ad2c54aad91fca8409d54da4494627a41120491
kuatov2002 <channelgamer58@gmail.com>
2025-10-24 22:43:08 +0500

    hp image

--- Assets/_Project/Scripts/MainCharacter.cs
+++ Assets/_Project/Scripts/MainCharacter.cs
@@ -1,25 +1,34 @@
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class MainCharacter : MonoBehaviour, IDamageable
 {
     public float maxHp = 100f;
     public float currentHp = 100f;
 
+    public Image hp;
     private void Start()
     {
         currentHp = maxHp;
+        UpdateHpBar();
     }
     
     public void TakeDamage(float damage)
     {
         currentHp -= damage;
-
+        UpdateHpBar();
         if (currentHp <= 0)
         {
             Die();
         }
     }
 
+    private void UpdateHpBar()
+    {
+        hp.fillAmount = currentHp / maxHp;
+    }
+    
     private void Die()
     {
         Debug.Log("я умер");
