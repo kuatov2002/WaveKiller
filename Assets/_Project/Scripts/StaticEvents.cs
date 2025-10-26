b5894bc324eacefb4c6e5d82f3c2b7661c59ddea
kuatov2002 <channelgamer58@gmail.com>
2025-10-27 02:15:14 +0500

    cutscene fixes

--- Assets/_Project/Scripts/StaticEvents.cs
+++ Assets/_Project/Scripts/StaticEvents.cs
@@ -2,6 +2,6 @@ using System;
 
 public static class StaticEvents
 {
-    public static Action<bool> OnGameOver;
+    public static Action<bool> OnGameOver = delegate { };
     public static bool IsGameOver;
 }
