22ef08a6aec212f9e7853bac4e34a5b956a1471f
kuatov2002 <channelgamer58@gmail.com>
2025-10-26 17:48:31 +0500

    new scenes and bug fixes

--- Assets/_Project/Scripts/SceneLoader.cs
+++ Assets/_Project/Scripts/SceneLoader.cs
@@ -1,10 +1,21 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public string sceneName;
+    
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
